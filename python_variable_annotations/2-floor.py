77fb0f971f24e0d7058adf38513caa07625e8f29
GuarickGit <10615@holbertonstudents.com>
2025-07-28 10:46:07 +0200

    Add 2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module that provides a function to return the floor of a float."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a floating-point number.
+
+    Args:
+        n (float): The number to floor.
+
+    Returns:
+        int: The largest integer less than or equal to n.
+    """
+    return (math.floor(n))
