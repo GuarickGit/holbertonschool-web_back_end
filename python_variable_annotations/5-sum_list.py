ab1a605d9f272f8f0f76ee3dede1d8f27b5107b0
GuarickGit <10615@holbertonstudents.com>
2025-07-28 11:13:23 +0200

    Update | Add drom typing import List

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 """Module that provides a function to compute the sum of a list of floats."""
 
+from typing import List
 
-def sum_list(input_list: list) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """
     Calculates the sum of all float elements in a list.
 
     Args:
-        input_list (list): A list of floats.
+        input_list (List[float]): A list of floats.
 
     Returns:
         float: The sum of the elements in the list.
