11a674198149c6da8b4d22df1261f947a1bc333b
GuarickGit <10615@holbertonstudents.com>
2025-07-29 10:00:22 +0200

    Update | pycodestyle

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -24,12 +24,12 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
     # Création des tâches asynchrones avec wait_random
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
 
-    delays = [] # Liste pour stocker les délais au fur et à mesure
+    delays = []  # Liste pour stocker les délais au fur et à mesure
 
     # asyncio.as_completed retourne les tâches dans
     # l'ordre où elles se terminent
     for task in asyncio.as_completed(tasks):
-        delay = await task # On attend que la tâche soit terminée
-        bisect.insort(delays, delay) # Insertion triée du délai dans la liste
+        delay = await task  # On attend que la tâche soit terminée
+        bisect.insort(delays, delay)  # Insertion triée du délai dans la liste
 
-    return (delays) # On retourne la liste finale triée des délais
+    return (delays)  # On retourne la liste finale triée des délais
