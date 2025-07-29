8a17f348cd5187915dc4fcb233e6ae1411f1e129
GuarickGit <10615@holbertonstudents.com>
2025-07-29 09:55:15 +0200

    Update | Add comments

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,6 +5,7 @@ and return sorted delays."""
 import asyncio
 import bisect
 
+# Importation de la fonction wait_random définie dans un autre fichier
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
@@ -20,12 +21,15 @@ async def wait_n(n: int, max_delay: int) -> list[float]:
     Returns:
         list[float]: Sorted list of delays from all tasks.
     """
+    # Création des tâches asynchrones avec wait_random
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
 
-    delays = []
+    delays = [] # Liste pour stocker les délais au fur et à mesure
 
+    # asyncio.as_completed retourne les tâches dans
+    # l'ordre où elles se terminent
     for task in asyncio.as_completed(tasks):
-        delay = await task
-        bisect.insort(delays, delay)
+        delay = await task # On attend que la tâche soit terminée
+        bisect.insort(delays, delay) # Insertion triée du délai dans la liste
 
-    return (delays)
+    return (delays) # On retourne la liste finale triée des délais
