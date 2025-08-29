f8a3ccb1e07ce8ddf36d1454046de57dcb73883b
GuarickGit <10615@holbertonstudents.com>
2025-08-29 11:29:39 +0200

    Add 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -3,4 +3,11 @@ import uploadPhoto from "./5-photo-reject.js";
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => result.map((result) => {
+      if (result.status === "fulfilled") {
+        return { status: 'fulfilled', value: result.value };
+      }
+
+      return { status: 'rejected', value: result.reason.toString() };
+    }));
 }
