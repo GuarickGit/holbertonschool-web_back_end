17caddef984e9cca189099c54b7af67328adbae7
GuarickGit <10615@holbertonstudents.com>
2025-09-01 13:53:45 +0200

    Add 2-read_file.js

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+function countStudents(path) {
+  const fs = require('fs');
+
+  let data;
+  try {
+    data = fs.readFileSync(path, { encoding: 'utf8' });
+  } catch (e) {
+    throw new Error('Cannot load the database');
+  }
+
+  const splittedData = data.split('\n');
+
+  const lines = splittedData.slice(1).filter((line) => line.trim() !== '');
+
+  const students = lines.map((line) => line.split(','));
+
+  const groups = {};
+
+  students.forEach((student) => {
+    const firstname = student[0];
+    const field = student[student.length - 1];
+    if (!groups[field]) {
+      groups[field] = [];
+    }
+    groups[field].push(firstname);
+  });
+
+  console.log(`Number of students: ${students.length}`);
+  for (const field in groups) {
+    console.log(`Number of students in ${field}: ${groups[field].length}. List: ${groups[field].join(', ')}`);
+  }
+}
+
+module.exports = countStudents;
