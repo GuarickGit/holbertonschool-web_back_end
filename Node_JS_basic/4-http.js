e23be878977a14aa9694cec96e09a1290f618b7e
GuarickGit <10615@holbertonstudents.com>
2025-09-02 13:41:02 +0200

    Update | From scratch

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,13 +1,11 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-type': 'text/plain' });
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   res.end('Hello Holberton School!');
 });
 
-const PORT = 1245;
+app.listen(1245);
 
-app.listen(PORT, 'localhost', () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+module.exports = app;
